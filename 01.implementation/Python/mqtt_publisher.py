6341e1ae1996f5eb0b7baa69f371d45e4504c793
matteo-martinelli <wmatte2005@gmail.com>
2023-03-29 11:55:11 +0200

    Defined publish telemetry data func

--- 01.implementation/Python/mqtt_publisher.py
+++ 01.implementation/Python/mqtt_publisher.py
@@ -9,6 +9,7 @@ O_10: compressor.
 
 from conf.mqtt_conf_parameters import MqttConfiguratorParameter
 import paho.mqtt.client as mqtt
+import json
 
 class MqttPublisher(object):
     """Mqtt publisher class to publish mqtt topics."""
@@ -20,8 +21,10 @@ class MqttPublisher(object):
 
     def open_connection(self):
         self.mqtt_client.on_connect = self.on_connect
+        
         #mqtt_client.username_pw_set(MqttConfiguratorParameter.MQTT_USER,
         #                            MqttConfiguratorParameter.MQTT_PW)
+        
         self.mqtt_client.connect(MqttConfiguratorParameter.BROKER_ADDRESS, 
                                 MqttConfiguratorParameter.BROKER_PORT)
         self.mqtt_client.loop_start()
@@ -30,7 +33,9 @@ class MqttPublisher(object):
         self.mqtt_client.loop_stop()
         print('Mqtt Loop stopped')
 
-    def publish_telemetry_data():
-        target_topic = '{0}/{1}/'
-        pass    
-
+    def publish_telemetry_data(self, topic: str, target_payload: json):
+        target_topic = 'user:{0}/{1}/'.format(MqttConfiguratorParameter.MQTT_USER, 
+                                         topic)
+        #target_payload = payload.to_json()
+        self.mqtt_client.publish(target_topic, target_payload, 0, True)
+        print(f"Info Published: Topic: {target_topic} Payload: {target_payload}")
