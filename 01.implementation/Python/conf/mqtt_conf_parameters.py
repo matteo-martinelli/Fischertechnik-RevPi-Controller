841affe78ba321e32afcc779573b6703b8ee0a55
matteo-martinelli <wmatte2005@gmail.com>
2023-03-29 11:56:08 +0200

    Changed mqtt_conf_parameters Changed user.

--- 01.implementation/Python/conf/mqtt_conf_parameters.py
+++ 01.implementation/Python/conf/mqtt_conf_parameters.py
@@ -9,6 +9,6 @@ Stores the mqtt broker connection global variables
 class MqttConfiguratorParameter(object):
     BROKER_ADDRESS = '192.168.137.125'
     BROKER_PORT = 1883
-    MQTT_USER = 'put here the user'
+    MQTT_USER = 'dept_manager'
     MQTT_PW = 'put here the pw'
-    MQTT_BASIC_TOPIC = '/iot/user_{0}'.format(MQTT_USER)    
+    MQTT_BASIC_TOPIC = '/user_{0}'.format(MQTT_USER)    
