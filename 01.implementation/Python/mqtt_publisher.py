b372ab5c779765f0e1b5d537a5154bdd995e7352
wmex91 <matteo.martinelli.1991@gmail.com>
2023-07-07 11:28:09 +0200

    Minor changes

--- 01.implementation/Python/mqtt_publisher.py
+++ 01.implementation/Python/mqtt_publisher.py
@@ -8,6 +8,7 @@ O_10: compressor.
 """
 
 
+import time
 from conf.mqtt_conf_parameters import MqttConfiguratorParameter
 import paho.mqtt.client as mqtt
 import json
@@ -18,26 +19,29 @@ class MqttPublisher(object):
         self.mqtt_client = mqtt.Client()
 
     def on_connect(self, client , userdata , flags , rc):
-        print('Mqtt connected with result code ' + str(rc))
+        print('Mqtt publisher client connected with result code ' + str(rc))
 
     def open_connection(self):
         self.mqtt_client.on_connect = self.on_connect
-        
-        #mqtt_client.username_pw_set(MqttConfiguratorParameter.MQTT_USER,
-        #                            MqttConfiguratorParameter.MQTT_PW)
-        
-        self.mqtt_client.connect(MqttConfiguratorParameter.BROKER_ADDRESS, 
-                                MqttConfiguratorParameter.BROKER_PORT)
-        self.mqtt_client.loop_start()
 
+        #mqtt_configured_user = MqttConfiguratorParameter.MQTT_USER
+        #mqtt_configured_pw = MqttConfiguratorParameter.MQTT_PW
+        mqtt_configured_address = MqttConfiguratorParameter.BROKER_ADDRESS
+        mqtt_configured_port = MqttConfiguratorParameter.BROKER_PORT
+
+        #mqtt_client.username_pw_set(mqtt_configured_user,mqtt_configured_pw)
+        self.mqtt_client.connect(mqtt_configured_address, mqtt_configured_port)
+        self.mqtt_client.loop_start()
+        
     def close_connection(self):
         self.mqtt_client.loop_stop()
-        print('Mqtt Loop stopped')
+        print('Mqtt publisher client Loop stopped')
 
+    # TODO: set the configuration retain flag
     def publish_telemetry_data(self, topic: str, target_payload: json):
         if (MqttConfiguratorParameter.ACTIVE_MQTT == True):
-            target_topic = 'user:{0}/{1}/'.format(MqttConfiguratorParameter.MQTT_USER, 
-                                            topic)
+            mqtt_configured_user = MqttConfiguratorParameter.MQTT_USER
+            target_topic = 'user:{0}/{1}/'.format(mqtt_configured_user, topic)
             #target_payload = payload.to_json()
             self.mqtt_client.publish(target_topic, target_payload, 0, True)
             #print(f"Info Published: Topic: {target_topic} Payload: {target_payload}")
