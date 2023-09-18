7555aa0ae217606ab48e99c1ef66a6b0d73eb74f
wmex91 <matteo.martinelli.1991@gmail.com>
2023-09-18 17:46:42 +0200

    Machine configurations class updated
    Added class configuration description; udpated the associated initial
    values for speed configurations: moved from number notation to str
    notation (Low, Medium, High).

--- src/machines/configurations/default_station_configs.py
+++ src/machines/configurations/default_station_configs.py
@@ -4,15 +4,38 @@
 default_station_configs.py: DefaultStationsConfigs class
 
 Contains constants for initialising machine stations configurations
+
+The configuration stored here set various aspects of the Fischertechnik runtime
+behaviours. 
+
+  - PIECES_TO_PRODUCE:          sets pieces to produce; this value is compared 
+                                to the variable that counts pieces done to 
+                                understand if the batch requested is completed
+  - COMPRESSOR_BEHAVIOUR:       always_on => always on during operations
+                                on_request => turned on and off when requested 
+                                something_else => another value (used only for 
+                                dev and test purposes)
+  - OVEN_PROCESSING_TIME:       1 => seconds to keep the product in heating 
+                                phase at target temp
+  - SAW_PROCESSING_TIME:        1 => seconds to keep the product in heating 
+                                phase at target temp
+  - VACUUM_CARRIER_SPEED:       speed at which the carrier works; the speed 
+                                increase and decrease is simulated through one 
+                                or more stops; target values are "low", 
+                                "medium", "high
+  - TURNTABLE_CARRIER_SPEED:    speed at which the carrier works; the speed 
+                                increase and decrease is simulated through one 
+                                or more stops; target values are "low", 
+                                "medium", "high
 """
 
 class DefaultStationsConfigs(object):
-    PIECES_TO_PRODUCE = 4
-    COMPRESSOR_BEHAVIOUR = 'something_else'
-    OVEN_PROCESSING_TIME = 1
-    SAW_PROCESSING_TIME = 1
-    VACUUM_CARRIER_SPEED = 1
-    TURNTABLE_CARRIER_SPEED = 1
+    PIECES_TO_PRODUCE = 4                       # pieces
+    COMPRESSOR_BEHAVIOUR = 'something_else'     # string
+    OVEN_PROCESSING_TIME = 1                    # seconds
+    SAW_PROCESSING_TIME = 1                     # seconds
+    VACUUM_CARRIER_SPEED = "High"               # string (to set)
+    TURNTABLE_CARRIER_SPEED = 1                 # string (to set)
 
     def __init__(self):
         self.description = 'Default configuration values for multiproc_dept'
